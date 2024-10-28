33423c2b95e9a70419cb7a8c6211363115459155
Max Cochran <wolfstain12@gmail.com>
2024-10-28 09:27:00 +0300

    подключаем pages

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,6 +14,7 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'docs'),
+    publicPath: './',  // Путь для статических файлов
     clean: true
   },
   module: {

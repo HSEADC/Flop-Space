bcc79554028b995a7eab0849a16f8e6b2caaeca2
Max Cochran <wolfstain12@gmail.com>
2024-12-03 12:40:26 +0300

    добавлены разделы

--- config/html-pages.config.js
+++ config/html-pages.config.js
@@ -3,10 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // Массив с описанием страниц
 const pages = [
   { template: './src/index.html', filename: './index.html' },
-  { template: './src/cemetery/cemetery.html', filename: './cemetery.html' },
-  { template: './src/advices/advices.html', filename: './advices.html' },
+  { template: './src/cases/cases.html', filename: './cases.html' },
+  { template: './src/networking/networking.html', filename: './networking.html' },
+  { template: './src/tests/tests.html', filename: './tests.html' },
+  { template: './src/media/media.html', filename: './media.html' },
   { template: './src/about/about.html', filename: './about.html' },
-  { template: './src/cemetery/articles/about_dead_startup.html', filename: './about_dead_startup.html' }
+  { template: './src/media/articles/about_dead_startup.html', filename: './about_dead_startup.html' }
 ];
 
 // Генерация плагинов для каждой страницы

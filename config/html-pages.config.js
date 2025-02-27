dffc6809ee49096aa5e609c321e7a1616fc7e510
Max Cochran <wolfstain12@gmail.com>
2025-02-27 15:37:21 +0300

    весь проект очищен с сохранением старой структуры

--- config/html-pages.config.js
+++ config/html-pages.config.js
@@ -3,12 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // Массив с описанием страниц
 const pages = [
   { template: './src/index.html', filename: './index.html' },
-  { template: './src/cases/cases.html', filename: './cases.html' },
-  { template: './src/styleguide/styleguide.html', filename: './styleguide.html' },
-  { template: './src/tests/tests.html', filename: './tests.html' },
-  { template: './src/media/media.html', filename: './media.html' },
-  { template: './src/about/about.html', filename: './about.html' },
-  { template: './src/media/articles/about_dead_startup.html', filename: './about_dead_startup.html' }
+  { template: './src/pages/cases/cases.html', filename: './cases.html' },
+  { template: './src/pages/styleguide/styleguide.html', filename: './styleguide.html' },
+  { template: './src/pages/tests/tests.html', filename: './tests.html' },
+  { template: './src/pages/media/media.html', filename: './media.html' },
+  { template: './src/pages/about/about.html', filename: './about.html' },
+  { template: './src/pages/media/articles/about_dead_startup.html', filename: './about_dead_startup.html' }
 ];
 
 // Генерация плагинов для каждой страницы
